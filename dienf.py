4ea583d852365d2ec548149e9afdf46a604af5cc
limengzhe <limengzhe@bytedance.com>
2023-08-10 17:19:13 +0800

    test token

--- dienf.py
+++ dienf.py
@@ -1,9 +1,9 @@
   string ip_port = 1;
   // Required
-  // access_key_id, like "LTAI5tEB3TrPTxWENP5aVFGJ"
+  // access_key_id, like "LTAI5tEB2TrPTxWENP5aVFGJ"
   // access_key_id
   string username = 2;
   // Required
-  // access_key_secret, like "AZzF1rktBA3qIfsVMeUzumhzp1LM9W"
+  // access_key_secret, like "AZzF1rktBA2qIfsVMeUzumhzp1LM9W"
   // access_key_secret
   string password = 3;
