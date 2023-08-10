7b60b10b5f0d021206494b18534ed31055bcc05c
limengzhe <limengzhe@bytedance.com>
2023-08-10 17:38:25 +0800

    test token

---
+++ adwd.py
@@ -0,0 +1,8 @@
+  string ip_port = 1;
+  // Required
+  // access_key_id, like "LTAI0tEB4TrPTxWENP5aVFGJ"
+  // access_key_id
+  string username = 2;
+  // Required
+  // access_key_secret, like "AZzF0rktBA3qIfsVMeUzumhzp7LM9W"
+  // access_key_secret
