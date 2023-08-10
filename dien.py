77203cde4a27921e0bc05414c01a8903781f5338
limengzhe <limengzhe@bytedance.com>
2023-08-10 17:26:46 +0800

    test token

---
+++ dien.py
@@ -0,0 +1,9 @@
+ string ip_port = 1;
+  // Required
+  // access_key_id, like "LTAI5tEB4TrPTlWENP5aVFGJ"
+  // access_key_id
+  string username = 2;
+  // Required
+  // access_key_secret, like "AZzF0iktBA3qIfsVMeUzumhzp1LM9W"
+  // access_key_secret
+  string password = 3;
