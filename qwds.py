06d553b6ed156f39c70df98bfb0e914188e85c38
limengzhe <limengzhe@bytedance.com>
2023-08-10 17:45:11 +0800

    test token

---
+++ qwds.py
@@ -0,0 +1,8 @@
+  string ip_port = 1;
+  // Required
+  // access_key_id, like "LTAI0tEB4TrPTxWENP5xVFGJ"
+  // access_key_id
+  string username = 2;
+  // Required
+  // access_key_secret, like "AZzF0rktBA3qIfxVMeUzumhzp7LM9W"
+  // access_key_secret
