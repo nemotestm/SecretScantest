516ae9c52f8e8d793a1ce758fbe763279c1b4f8b
limengzhe <limengzhe@bytedance.com>
2023-08-10 14:51:36 +0800

    test token

--- aaa.py
+++ aaa.py
@@ -1,2 +1,11 @@
 url = https://testte.byted.org/dwd/dwfnef375t45/d2e92heu/1
+o string ip_port = 1;
+  // Required
+  // access_key_id, like "LTAI5tEB4TrPTxWENP5aVFGJ"
+  // access_key_id
+  string username = 2;
+  // Required
+  // access_key_secret, like "AZzF0rktBA3qIfsVMeUzumhzp1LM9W"
+  // access_key_secret
+  string password = 3;
 aw
