454b22a3300dbbe9d4f3519b4ae9c3e76e79b237
limengzhe <limengzhe@bytedance.com>
2023-08-10 14:55:49 +0800

    test token

--- aaa.py
+++ aaa.py
@@ -3,14 +3,12 @@ o string ip_port = 1;
   // Required
   // access_key_id, like "
 dwfdwd
-LTAI5tEB3TrPTxWENP5aVFGJ
   // access_key_id
   string username = 2;
   // Required
   // access_key_secret, like
 
 afdwf
-AZzF1rktBA3qIfsVMeUzumhzp1LM9W
   // access_key_secret
   string password = 3;
 aw
