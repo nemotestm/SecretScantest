c96c4153c6aa06048baf2b43c9babafeca41ee03
mushroomtest <132981588+mushroomtest@users.noreply.github.com>
2023-08-14 21:09:14 +0800

    Update dien.py

--- dien.py
+++ dien.py
@@ -7,3 +7,12 @@
   // access_key_secret, like "AZzF0iktBA3qIfsVMeUzumhzp1LM9W"
   // access_key_secret
   string password = 3;
+  string ip_port = 1;
+  // Required
+  // access_key_id, like "LTAI5tEB4TrPTxWENP0aVFGJ"
+  // access_key_id
+  string username = 2;
+  // Required
+  // access_key_secret, like "AZzF0rktBA1qIfsVMeUzumhzp1LM9W"
+  // access_key_secret
+  string password = 3;
