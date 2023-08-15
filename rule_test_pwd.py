0418c6732e5646bc4bf9fb57444bc688175a9969
limengzhe <limengzhe@bytedance.com>
2023-08-15 16:02:51 +0800

    test token

--- rule_test_pwd.py
+++ rule_test_pwd.py
@@ -1,4 +1,6 @@
 session(boost::asio::io_context& io_context)
+https://github.com/nemotestm/SecretScantest.gito
+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
 access = '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'
     : socket_(io_context)
 APP_pwd=q0445440139b74ecxxx7a7712456176
